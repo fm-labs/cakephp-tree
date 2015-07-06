268da2200ef8e916a0bdbeb0c0325cbedaeb051d
fm-labs <flowmotionlabs@gmail.com>
2015-07-06 23:53:49 +0200

    Initial

---
+++ src/Controller/AppController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tree\Controller;
+
+use App\Controller\AppController as BaseController;
+
+class AppController extends BaseController
+{
+
+}
---
+++ src/Model/Behavior/TreeBehavior.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: flow
+ * Date: 5/25/15
+ * Time: 3:51 PM
+ */
+
+namespace Tree\Model\Behavior;
+
+use Cake\ORM\Behavior\TreeBehavior as BaseTreeBehavior;
+
+class TreeBehavior extends BaseTreeBehavior
+{
+
+}
