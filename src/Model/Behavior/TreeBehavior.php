c89e20263436c54fc27e5eeb6c783e7e886786d8
fm-labs <flowmotionlabs@gmail.com>
2016-04-16 23:48:11 +0200

    added fixed version of _getMax() method

--- src/Model/Behavior/TreeBehavior.php
+++ src/Model/Behavior/TreeBehavior.php
@@ -12,5 +12,27 @@ use Cake\ORM\Behavior\TreeBehavior as BaseTreeBehavior;
 
 class TreeBehavior extends BaseTreeBehavior
 {
+    /**
+     * Fixed version for CakePHP 3.1.x branch:
+     *  Orders by right field instead of left field.
+     * This got fixed in the 3.2.x branch
+     *
+     * Returns the maximum index value in the table.
+     *
+     * @return int
+     */
+    protected function _getMax()
+    {
+        $field = $this->_config['right'];
+        $edge = $this->_scope($this->_table->find())
+            ->select([$field])
+            ->orderDesc($field)
+            ->first();
 
+        if (empty($edge->{$field})) {
+            return 0;
+        }
+
+        return $edge->{$field};
+    }
 }
