877c39333d8d59574aab2df0caa970e0d3ba5548
fm-labs <flowmotionlabs@gmail.com>
2020-04-11 23:41:47 +0200

    updated phpunit config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,31 @@
 declare(strict_types=1);
 
 /**
- * Test runner bootstrap.
+ * Test suite bootstrap.
  *
- * Add additional configuration/setup your application needs when running
- * unit tests in this file.
+ * This function is used to find the location of CakePHP whether CakePHP
+ * has been installed as a dependency of the plugin, or the plugin is itself
+ * installed as a dependency of an application.
  */
-require dirname(__DIR__) . '/config/bootstrap.php';
+$findRoot = function ($root) {
+    do {
+        $lastRoot = $root;
+        $root = dirname($root);
+        if (is_dir($root . '/vendor/cakephp/cakephp')) {
+            return $root;
+        }
+    } while ($root !== $lastRoot);
+
+    throw new Exception("Cannot find the root of the application, unable to run tests");
+};
+$root = $findRoot(__FILE__);
+unset($findRoot);
+
+chdir($root);
+if (file_exists($root . '/config/bootstrap.php')) {
+    require $root . '/config/bootstrap.php';
+
+    return;
+}
+
+require $root . '/vendor/cakephp/cakephp/tests/bootstrap.php';
