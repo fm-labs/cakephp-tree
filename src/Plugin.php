385745f3d9f7a9f91865e69cd47245ec5428ac3c
fm-labs <flowmotionlabs@gmail.com>
2023-03-14 13:47:38 +0100

    Added Plugin class Removed VERSION.txt

---
+++ src/Plugin.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Tree;
+
+/**
+ * Backward compatibility plugin class.
+ *
+ * @deprecated Use TreePlugin class instead.
+ */
+class Plugin extends TreePlugin
+{
+}
---
+++ src/TreePlugin.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Tree;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Tree plugin for CakePHP
+ */
+class TreePlugin extends BasePlugin
+{
+    /**
+     * @var bool
+     */
+    public $routesEnabled = false;
+
+    /**
+     * @var bool
+     */
+    public $bootstrapEnabled = false;
+}
